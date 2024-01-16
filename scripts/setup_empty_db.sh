7510f33c99674cab5807cb820d63bd4941a77cd4
Ernie Hershey <github@ernie.org>
2024-01-16 11:11:12 -0500

    setup script fixup

--- scripts/setup_empty_db.sh
+++ scripts/setup_empty_db.sh
@@ -3,5 +3,5 @@
 # DROP "nodes" COLLECTION IN "test" DB
 echo "db.nodes.drop()" | mongo test
 
-mongo test "$(dirname "$0")"/createIndexes.js || echo aborting on error ; exit 1
-mongo test "$(dirname "$0")"/loadNodes.js || echo aborting on error ; exit 1
+mongo test "$(dirname "$0")"/createIndexes.js || { echo aborting on error ; exit 1 ; }
+mongo test "$(dirname "$0")"/loadNodes.js || { echo aborting on error ; exit 1 ; } 
