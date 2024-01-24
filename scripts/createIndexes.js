a79d478241a49e05349f139177dbb34074ae1726
Ernie Hershey <github@ernie.org>
2024-01-24 16:19:51 -0500

    mongosh exits with non-zero on error

--- scripts/createIndexes.js
+++ scripts/createIndexes.js
@@ -1,18 +1,12 @@
 result = db.gps_log.ensureIndex({ entry_date: 1, entry_source: 1, accuracy: 1 }, { unique: true, dropDups: true });
 printjson(result);
-if(!result.ok) quit(1)
 result = db.gps_log.ensureIndex( { loc : "2dsphere" } );
 printjson(result);
-if(!result.ok) quit(1)
 result = db.gps_log.ensureIndex({ entry_source: 1 });
 printjson(result);
-if(!result.ok) quit(1)
 result = db.nodes.ensureIndex(   {  loc: '2dsphere' } );
 printjson(result);
-if(!result.ok) quit(1)
 result = db.nodes.ensureIndex( { external_id: 1 },  { unique: true });
 printjson(result);
-if(!result.ok) quit(1)
 result = db.jobs.ensureIndex({ job_id: 1 }, { unique: true });
 printjson(result);
-if(!result.ok) quit(1)
